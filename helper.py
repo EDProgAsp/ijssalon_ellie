2f2907415a0d1dde2378be149ff8bf8e5b2ea9db
Esther Dorland <edorland@zorgned.nl>
2024-03-17 16:04:44 +0100

    Verschillende functies, hoofdprogramma en boekhouding toegevoegd

--- helper.py
+++ helper.py
@@ -5,3 +5,26 @@ def decoreer(tekst = ""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag, personen):
+    try:
+        bedrag_pp = bedrag / personen
+        bedrag_pp1 = format(bedrag_pp, '.2f')
+        bedrag_pp2 = bedrag_pp1.replace(".", ",")
+    except:
+        bedrag_pp2 = "??"
+    uitvoer = f"Het bedrag per persoon is {bedrag_pp2} euro."
+    return uitvoer
+
+def onderstreep(tekst = ""):
+    lengte = len(tekst)
+    uit = []
+    uit.append(tekst)
+    uit.insert(1, (lengte * "="))
+    return uit
+
+def som(dictionary):
+    totaal = 0
+    for v in dictionary.values():
+        totaal += v
+    return totaal
---
+++ presentatie.py
@@ -0,0 +1,7 @@
+def presenteer(dictionary, totaal):
+    for k, v in dictionary.items():
+        print(f"{k} : {v} euro")
+    
+    print(26 * "=")
+
+    print(f"Totaal : {totaal} euro")
